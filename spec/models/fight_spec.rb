499a14bb3925ec27242e76a41fa1e5861d8f2b55
pascal <pascal@debian>
2015-09-13 18:30:15 +0200

    change create to build

--- spec/models/fight_spec.rb
+++ spec/models/fight_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 RSpec.describe Fight, :type => :model do
-  let(:fight1) {FactoryGirl.create(:fight)}
+  let(:fight1) {FactoryGirl.build(:fight)}
   it { expect(subject).to validate_presence_of :pet1 }
   it { expect(subject).to validate_presence_of :pet2 }
   it { expect(subject).to validate_presence_of :win1 }
